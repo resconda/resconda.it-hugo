b0677c5f8b7ada96881a6a92570720d8f08afd0e
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-09-04 18:37:27 +0200

    document scroll event handler
    For navbar and fadein-scroll page elements

---
+++ static/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".fadein-scroll");
+
+    if(pageTop > 0){
+        $('nav.navbar').addClass('background');
+    }else{
+        $('nav.navbar').removeClass('background');
+
+    }
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if ($(tag).offset().top < pageBottom) {
+            $(tag).addClass("visible");
+        } 
+        // else {
+        //     $(tag).removeClass("visible");
+        // }
+    }
+}).on("ready", function () {
+    setTimeout(() => {
+        $('.fadein-onload').addClass("visible");
+    }, 500);
+});
