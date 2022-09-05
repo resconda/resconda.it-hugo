c2f7bd5e68c68c66d1ffb89800da00c06a1d1f58
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-09-05 19:43:21 +0200

    introducing Lodash library

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,11 @@
-$(document).on("scroll", function () {
+var scrollUtils = function () {
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
     var tags = $(".fadein-scroll");
 
-    if(pageTop > 0){
+    if (pageTop > 0) {
         $('nav.navbar').addClass('background');
-    }else{
+    } else {
         $('nav.navbar').removeClass('background');
 
     }
@@ -13,12 +13,21 @@ $(document).on("scroll", function () {
         var tag = tags[i];
         if ($(tag).offset().top < pageBottom) {
             $(tag).addClass("visible");
-        } 
+        }
         // else {
         //     $(tag).removeClass("visible");
         // }
     }
-}).on("ready", function () {
+    var peopleWalkingVideo = $('#peopleWalkingVideo');
+    if (peopleWalkingVideo.offset().top < pageBottom && peopleWalkingVideo.offset().top + peopleWalkingVideo.height() > pageTop) {
+        peopleWalkingVideo[0].play();
+    } else {
+        peopleWalkingVideo[0].pause();
+    }
+};
+$(document)
+.on("scroll", _.throttle(scrollUtils, 100))
+.on("ready", function () {
     setTimeout(() => {
         $('.fadein-onload').addClass("visible");
     }, 500);
