5dea4f309dc0a2808edb44eb0c169e8c103e77ec
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2022-12-10 20:44:41 +0100

    introducing cgi scripts

---
+++ static/cgi/form_contact.py
@@ -0,0 +1,65 @@
+from mod_python import apache
+from json import dumps as jds
+import urllib.parse as ups
+
+VALID_PARAMS = [
+    ("name", False, _sanitize_string),
+    ("email", True, _sanitize_string),
+    ("phone", False, _sanitize_string),
+    ("message", False, _sanitize_string),
+    ("newsletter", True, _sanitize_bool),
+]
+
+
+def _sanitize_bool(input) -> bool:
+    if type(input) is bool:
+        return input
+    if type(input) is int:
+        return input != 0
+    if type(input) is str:
+        return input != "0"
+
+def _sanitize_string(input) -> str:
+    if type(input) in [str, bytes]:
+        return ups.unquote_plus(input)
+    else:
+        return None
+
+def validate_input(params: dict) -> list:
+    output = []
+    for vp in VALID_PARAMS:
+        pname = vp[0]
+        prequired = vp[1]
+        if prequired and pname not in params:
+            output.append({pname: "Missing required"})
+    return output
+
+def sanitise_input(params: dict) -> dict:
+    sanitised = {}
+    for vp in VALID_PARAMS:
+        pname = vp[0]
+        pval = params.get(pname)
+        if pval is not None:
+            phandler = vp[2]
+            sanitised[pname] = phandler(pval)
+    return sanitised
+
+def form_contact(req):
+    output = {
+        "errors": []
+    }
+    rawdata = req.read()
+    rawparams = ups.parse_qsl(rawdata)
+    validationErrors = validate_input(params=rawparams)
+    if len(validationErrors) > 0:
+        output["errors"] = validationErrors
+        return apache.OK
+    sanitised = sanitise_input(rawparams)
+    req.log_error("Sanitised input: %s" % jds(sanitised))
+    # req.log_error("Request content type %s" % req.content_type)
+    # req.log_error(rawdata.decode(),  apache.APLOG_ERR)
+    # TEMP
+    req.content_type = "application/json"
+    
+    req.write(jds(output))
+    return apache.OK
---
+++ static/cgi/router.py
@@ -0,0 +1,16 @@
+from mod_python import apache
+from form_contact import form_contact
+
+URIs = [
+    ("/cgi/form_contact.py", form_contact),
+]
+
+def handler(req):
+    request_uri = req.uri
+    for uriitem in URIs:
+        uri = uriitem[0]
+        urihandler = uriitem[1]
+        if uri == request_uri:
+            return urihandler(req)
+            break
+    return apache.HTTP_BAD_REQUEST
