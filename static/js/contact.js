1213ccf8b93e8fae460cdd7eed4d932c2a87b618
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2023-04-16 17:38:47 +0200

    wip contact form

--- static/js/contact.js
+++ static/js/contact.js
@@ -55,9 +55,10 @@ window.addEventListener("load", () => {
         var validatedData = validateForm();
         if(validatedData !== false){
             var XHR = new XMLHttpRequest();
-            var FD = new FormData();
+            var aData = []
             for(const [name, value] of Object.entries(validatedData)) {
-                FD.append(name, value);
+                // encValue = encodeURIComponent(value);
+                aData.push(`${name}=${encodeURIComponent(value)}`);
             };
             // Define what happens on successful data submission
             XHR.addEventListener('load', (event) => {
@@ -65,14 +66,14 @@ window.addEventListener("load", () => {
                 // console.log(JSON.stringify(data));
                 if (data.errors != undefined && data.errors.length > 0) {
                     data.errors.forEach(element => {
-                        errrow = $(`<div class="alert alert-warning">${element}</div>`);
-                        $("#form-response").append(errrow);
+                        errrow = new HTMLElement(); errrow.innerHTML = `<div class="alert alert-warning">${element}</div>`;
+                        document.getElementById("form-response").append(errrow);
                     });
                 } else {
                     let name = data.name;
-                    successmsg = $(`<div class="alert alert-info text-center">Grazie ${name}! Ti contatteremo presto.</div>`);
-                    $('#form-response').append(successmsg);
-                    $('#contactForm').find('input,textarea').val("");
+                    successmsg = new HTMLElement(); successmsg.innerHTML = `<div class="alert alert-info text-center">Grazie ${name}! Ti contatteremo presto.</div>`;
+                    document.getElementById('form-response').append(successmsg);
+                    document.getElementById('contactForm').querySelectorAll('input,textarea').forEach(el => { el.value = ""; });
                     defaultWidget.reset();
                 }
             });
@@ -83,7 +84,7 @@ window.addEventListener("load", () => {
             // Set up our request
             XHR.open('POST', '/cgi/form_contact');
             // Send our FormData object; HTTP headers are set automatically
-            XHR.send(FD);
+            XHR.send(aData.join("&"));
             // $.ajax({
             //     type: "POST",
             //     url: "/cgi/form_contact",
