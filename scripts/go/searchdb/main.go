5bd5cef9fd1b268f0959a6c3e6df3de5d2c46634
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2023-05-29 21:32:46 +0200

    RI-33 search backend wip

--- scripts/go/searchdb/main.go
+++ scripts/go/searchdb/main.go
@@ -58,7 +58,7 @@ var database *gorm.DB
 
 func initDB() {
 	var err error
-	database, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	database, err = gorm.Open(sqlite.Open("contents.sqlite"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
--- static/cgi/search.py
+++ static/cgi/search.py
@@ -1,10 +1,12 @@
 import sqlite3
 from mod_python import apache
 import os
+from json import dumps as jds
 
 SCRIPT_PATH = os.path.dirname(__file__)
-DB_PATH = os.path.join(SCRIPT_PATH, "res", "test.db")
-GENERIC_QUERY_TEMPLATE = '''
+DB_PATH = os.path.join(SCRIPT_PATH, "res", "contents.sqlite")
+
+GENERIC_QUERY_TEMPLATE = '''\
 SELECT id, category, title, summary, date, image_caption FROM db_contents WHERE 
 draft = 0
 AND ( 0
@@ -14,23 +16,81 @@ AND ( 0
     OR image_caption LIKE :caption
     )
 '''
+CATEGORIES_QUERY = '''\
+SELECT id, category FROM categories WHERE
+'''
 
-def _make_generic_query(term: str):
+def _dbcursor() -> sqlite3.cursor:
     con = sqlite3.connect(DB_PATH)
-    cur = con.execute(GENERIC_QUERY_TEMPLATE, {
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    return cur
+
+def _make_generic_query(term: str) -> list:
+    cur = _dbcursor()
+    ret = []
+    
+    for row in cur.execute(GENERIC_QUERY_TEMPLATE, {
         "category": f"%{term}%",
         "title": f"%{term}%",
         "summary": f"%{term}%",
         "caption": f"%{term}%",
-    })
-    
+    }):
+        ret.append(row)
+    return ret
 
     
-def search(req):
-    rawdata = req.read().decode()
-    req.log_error(rawdata,  apache.APLOG_DEBUG)
-    # parse_qs yields a dict whose values are all lists of values,
-    # so that multiple occurrences of a parameter are handled
-    # hence we must always handle lists, even when only one value is expected
-    rawparams = ups.parse_qs(rawdata)
-    req.log_error("Parsed params: %s" % str(rawparams), apache.APLOG_DEBUG)
+    """ 
+    GET /search?q={search_term}
+    """
+    def search(req):
+        rawdata = req.read().decode()
+        req.log_error(rawdata,  apache.APLOG_DEBUG)
+        # parse_qs yields a dict whose values are all lists of values,
+        # so that multiple occurrences of a parameter are handled
+        # hence we must always handle lists, even when only one value is expected
+        rawparams = ups.parse_qs(rawdata)
+        req.log_error("Parsed params: %s" % str(rawparams), apache.APLOG_DEBUG)
+        term = rawparams['q']
+        rows = _make_generic_query(term)
+        req.write(jds({
+            "term": term,
+            "data": rows
+        }))
+
+    """ 
+    GET /search/categories
+    """
+    def search_categories(req):
+        cursor = _dbcursor()
+        results = []
+        for row in cursor.execute("SELECT DISTINCT category FROM db_contents WHERE draft = 0"):
+            results.append(row['category'])
+        req.write({
+            "data": results
+        })
+
+    
+    """ 
+    GET /search/classes
+    """
+    def search_classes(req):
+        cursor = _dbcursor()
+        results = []
+        for row in cursor.execute("SELECT * FROM classes"):
+            results.append(row)
+        req.write({
+            "data": results
+        })
+    
+    """ 
+    GET /search/tags
+    """
+    def search_tags(req):
+        cursor = _dbcursor()
+        results = []
+        for row in cursor.execute("SELECT * FROM tags"):
+            results.append(row)
+        req.write({
+            "data": results
+        })
