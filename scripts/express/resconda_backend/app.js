1511b73fff7dcc6c0eae6cf7f71011541e083614
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2025-03-16 19:28:03 +0100

    completing express backend logic

--- scripts/express/resconda_backend/app.js
+++ scripts/express/resconda_backend/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MailchimpHandler = require("./mailchimp")
+const SendmailHelper = require('./sendmail')
+const WelcomeEmail = require('./welcome_email')
 app.use(bodyParser.json()) // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -18,14 +20,38 @@ app.route("/",)
   }
 })
 .post(async (req, res) => {
+  let mailchimpResponse;
   console.log(req.body);
   res.set("Content-Type", "application/json")
   try {
-    let response = await MailchimpHandler.addMember(req.body);
-    res.send(JSON.stringify(response));
+    mailchimpResponse = await MailchimpHandler.addMember(req.body);
   } catch (error) {
-    res.send({error: `Failed to register user: ${error.message}`}); 
+    res.send({ error: `Failed to register user: ${error.message}` });
+    return;
   }
+  // prepare and send feedback email
+  const contactName = req.body.name ?? "nuov@ iscritt@";
+  try {
+    SendmailHelper.sendMail(
+      mailchimpResponse.email_address,
+      "Ti sei iscritto alla newsletter di ResConDA",
+      WelcomeEmail.plain(contactName),
+      WelcomeEmail.html(contactName),
+    ).then((err, info) => {
+      let sendResponse;
+      if(err){
+        console.log(err);
+        sendResponse = err;
+      }else{
+        console.log(info)
+        sendResponse = info;
+      }
+      res.send(JSON.stringify(sendResponse));
+    });
+  } catch (error) {
+    res.send({ error: `Failed to send welcome mesasge to user: ${error.message}`});
+  }
+  
 })
 // DEBUG
 app.get("/list/:listId", async (req, res) => {
