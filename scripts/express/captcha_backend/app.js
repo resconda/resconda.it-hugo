87c652ebe9099e476c27994a696c817fd7248f28
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2025-03-22 18:27:19 +0100

    friendlycaptcha express backend

---
+++ scripts/express/captcha_backend/app.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import {FriendlyCaptchaHelper, ResultCodes } from './friendlycaptcha.js'
+
+const app = express()
+const CAPTCHAURL = "https://api.friendlycaptcha.com/api/v1/siteverify"
+
+const port = 3000
+
+app.route("/",)
+.get(async (req, res) => {
+  let solution = req.query["frc-captcha-solution"];
+  if(!solution){
+    res.status(400).send({error: "Invalid 'frc-captcha-solution' input"});
+  }
+  let verifyResult = await FriendlyCaptchaHelper.verify(solution);
+  let resBody = {};
+  switch (verifyResult) {
+    case ResultCodes.verifySuccess:
+        break;
+    case ResultCodes.verifyFail:
+        resBody = {error: "Captcha verification failed"};
+        break;
+    case ResultCodes.couldNotVerify:
+        resBody = {error: "Verification error"};
+        res.status(500);
+        break;
+    default:
+        resBody = {error: "Unexpected error"};
+        console.log(`Verify call returned error code ${verifyResult}`);
+        res.status(500);
+        break;
+  }
+  res.append("Content-Type", "application/json")
+    .send(resBody);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
---
+++ scripts/express/captcha_backend/friendlycaptcha.js
@@ -0,0 +1,27 @@
+import {  FriendlyCaptchaClient  } from "@friendlycaptcha/server-sdk";
+
+const frcClient = new FriendlyCaptchaClient({
+  apiKey: process.env.FRIENDLY_CAPTCHA_SECRET,
+  sitekey: process.env.FRIENDLY_CAPTCHA_SITEKEY,
+});
+
+export const ResultCodes = {
+    verifySuccess: 0,
+    verifyFail: 1,
+    couldNotVerify: 2,
+};
+
+export const FriendlyCaptchaHelper = {
+    verify: async (solution) => {
+        const result = await frcClient.verifyCaptchaResponse(solution);
+        if(result.wasAbleToVerify()){
+            if(result.shouldAccept()){
+                return ResultCodes.verifySuccess;
+            }else{
+                return ResultCodes.verifyFail;
+            }
+        }else{
+            return ResultCodes.couldNotVerify;
+        }
+    },
+}
