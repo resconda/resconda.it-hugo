e3e043e32d86b3cad3491b8c08e442a73d4c08bf
Paolo Marchetti <paolo.marchetti@resconda.it>
2026-08-08 17:30:16 +0200

    working script to download doc and images fom GDrive

--- scripts/pillole_publisher/gdrive.py
+++ scripts/pillole_publisher/gdrive.py
@@ -8,9 +8,12 @@ from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 
 from tempfile import NamedTemporaryFile, TemporaryDirectory
-from io import FileIO
+from io import open as fopen
+from shutil import rmtree, move, copytree
+from os import makedirs
 import argparse
 
+ROOT = os.path.dirname(os.path.abspath(__file__))
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 
@@ -23,11 +26,11 @@ class NoResultsException(Exception):
 
 def authenticate(credentialsfile, tokenfile):
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
+  # The file `tokenfile` stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
   if os.path.exists(tokenfile):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    creds = Credentials.from_authorized_user_file(tokenfile, SCOPES)
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
@@ -38,7 +41,7 @@ def authenticate(credentialsfile, tokenfile):
       )
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
-    with open("token.json", "w") as token:
+    with fopen(tokenfile, "w") as token:
       token.write(creds.to_json())
 
   return creds
@@ -56,19 +59,20 @@ def rescondaComunicazioneSharedDriveId(creds):
 def getDriveFile(pillole_gdocx, creds): 
   service = build("drive", "v3", credentials=creds)
   driveid = rescondaComunicazioneSharedDriveId(creds)
-  results = service.files().list(driveId=driveid, corpora="drive", includeItemsFromAllDrives=True, supportsAllDrives=True, q=f"name contains '{pillole_gdocx}'", fields="files(id,name,parents)").execute()
+  results = service.files().list(driveId=driveid, corpora="drive", includeItemsFromAllDrives=True, supportsAllDrives=True, q=f"name contains '{pillole_gdocx}' and mimeType = 'application/vnd.google-apps.document'", fields="files(id,name,parents)").execute()
   items = results.get("files", [])
   if len(items) == 0:
     raise NoResultsException(f"Could not find file '{pillole_gdocx}'")
   if len(items) > 1:
-    raise MultipleResultsException(f"Found more than one file '{pillole_gdocx}'")
+    stritems = ",".join([f"({item['name']}, {item['id']})" for item in items])
+    raise MultipleResultsException(f"Found more than one file '{pillole_gdocx}': {stritems}")
   return items[0]
 
 def fetchPilloleDocxMdx(pillole_gdocx, creds):
   the_file = getDriveFile(pillole_gdocx, creds)
   service = build("drive", "v3", credentials=creds)
   request = service.files().export(fileId=the_file["id"], mimeType="text/markdown")
-  tmpfile = NamedTemporaryFile()
+  tmpfile = NamedTemporaryFile(delete=False)
   downloader = MediaIoBaseDownload(tmpfile, request, chunksize=1024*1024)
   done = False
   print(f"Downloading Markdown-exported {pillole_gdocx}...")
@@ -88,8 +92,8 @@ def fetchImmagini(pillole_gdocx, creds):
   parents = the_file["parents"]
   if len(parents) != 1:
     raise Exception(f"File {pillole_gdocx} has {len(parents)} parents, expected 1")
-  parentid = parents[0]["id"]
-  results = service.files().list(driveId=driveid, corpora="drive", includeItemsFromAllDrives=True, supportsAllDrives=True, q=f"'{pId}' in parents and name = 'IMMAGINI'").execute()
+  parentid = parents[0]
+  results = service.files().list(driveId=driveid, corpora="drive", includeItemsFromAllDrives=True, supportsAllDrives=True, q=f"'{parentid}' in parents and name = 'IMMAGINI'").execute()
   items = results.get("files", [])
   if len(items) == 0:
     raise NoResultsException("Could not find folder IMMAGINI")
@@ -98,31 +102,38 @@ def fetchImmagini(pillole_gdocx, creds):
   the_folder = items[0]
   results = service.files().list(driveId=driveid, corpora="drive", includeItemsFromAllDrives=True, supportsAllDrives=True, q=f"'{the_folder['id']}' in parents", fields="files(id,name)").execute()
   items = results.get("files", [])
-  tmpdir = TemporaryDirectory()
+  tmpdir = TemporaryDirectory(delete=False)
   for item in items:
     print(f"Downloading image {item['name']}...")
     request = service.files().get_media(fileId=item["id"])
     tmpfile = os.path.join(tmpdir.name, item["name"])
-    downloader = MediaIoBaseDownload(tmpfile, request, chunksize=1024*1024)
-    done = False
-    while done is False:
-      status, done = downloader.next_chunk()
+    with fopen(tmpfile, "wb") as f:
+      downloader = MediaIoBaseDownload(f, request, chunksize=1024*1024)
+      done = False
+      while done is False:
+        status, done = downloader.next_chunk()
     print(f"Done")
-  return tmpdir.name
+  return tmpdir
   
 def main(args):
   creds = authenticate(args.creds, args.token)
-  pillole_docx_mdx = fetchPilloleDocxMdx(args.PILLOLE_GDOCX, creds)
-  print(f"pillole_docx_mdx: {pillole_docx_mdx}")
-  immagini_dir = fetchImmagini(args.PILLOLE_GDOCX, creds)
-  print(f"immagini_dir: {immagini_dir}")
+  builddir = os.path.join(ROOT, "build")
+  rmtree(builddir, ignore_errors=True)
+  makedirs(builddir, exist_ok=False)
+  pillole_docx_md = fetchPilloleDocxMdx(args.PILLOLE_GDOC, creds)
+  print(f"pillole_docx_md downloaded to: {pillole_docx_md}")
+  move(pillole_docx_md, os.path.join(builddir, args.PILLOLE_GDOC + ".md"))
+  immagini_dir = fetchImmagini(args.PILLOLE_GDOC, creds)
+  print(f"immagini_dir: {immagini_dir.name}")
+  copytree(immagini_dir.name, os.path.join(builddir, "IMMAGINI"))
+  immagini_dir.cleanup()
   
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("PILLOLE_GDOCX", help="The name of the 'pillole' Google Docx file to fetch")
-  parser.add_argument("-c","--creds", help="The path to the credentials file to use", default="credentials.json")
-  parser.add_argument("-t","--token", help="The path to the token JSON file to use", default="token.json")
+  parser.add_argument("PILLOLE_GDOC", help="The name of the 'pillole' Google Docx file to fetch")
+  parser.add_argument("-c","--creds", help="The path to the credentials file to use", default=os.path.join(ROOT, "credentials.json"))
+  parser.add_argument("-t","--token", help="The path to the token JSON file to use", default=os.path.join(ROOT, "token.json"))
 
   args = parser.parse_args()
   main(args)
